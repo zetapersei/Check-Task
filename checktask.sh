b54bc2513e160443a5ddb0967f043a44fc780ca5
Maurizio <zeta_persei@hotmail.com>
2018-07-21 17:36:21 +0200

    Update checktask.sh

--- checktask.sh
+++ checktask.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-user="user"
-check_now="`pidof <task name>`"
-echo $check_now > /home/$user/.checktask
-if [ !  "`cat -b /home/$user/.checktask`"  ]
-then
-sudo /etc/init.d/<task name> restart
-echo "The task <task name> is being restarted" | mail -s "Tak Restarded"  user@dominio.xx
+user="<user>" check_now="`pidof <task>`"
+ echo $check_now > /home/$user/.checktask
+if [ !  "`cat -b /home/$user/.checktask`" ]
+then 
+sudo /etc/init.d/<task> restart
+echo "The task <task> is being restarted" | mail -s "Task Restarded" user@domain
 fi
 exit
